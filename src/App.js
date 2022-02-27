d31516f8f92682fd56fa57f81fcfe21a02fcd421
Luis Da Silva <luisdanield@gmail.com>
2022-02-27 23:31:55 +0000

    update website

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <div className="spacer" />
       <footer>
         <p>App created by Luis Da Silva</p>
-        <p>Checkout more content at: <a href="https://luisds95.github.io/">luisds95.github.io/</a></p>
+        <p>Checkout more content at: <a href="https://luisdasilva.io/">luisdasilva.io/</a></p>
         <p>Get in touch at: <a href="mailto:luisdanield@gmail.com">luisdanield@gmail.com</a></p>
       </footer>
     </div>
